f9f11741380d48004e3b5c599e28a4fa3863f723
Lucas Fernandes <58313395+lucasfernande@users.noreply.github.com>
2023-04-13 17:27:16 -0300

    convertendo tipos

---
+++ secao3/aula6.py
@@ -0,0 +1,8 @@
+num = 5.5 
+print(type(num))
+
+numStr = str(num) # convertendo um número float para um número em texto (string)
+print(type(numStr))
+
+print(bool('')) # uma string vazia é igual a False
+print(bool(' ')) # uma string não-vazia é igual a True
