97eddb7092f365f178099fc3bc74b0a2b09daf62
Lucas Fernandes <58313395+lucasfernande@users.noreply.github.com>
2023-04-25 16:01:19 -0300

    testando o debugger do vs code

---
+++ secao3/aula36 - debugger.py
@@ -0,0 +1,11 @@
+condicao1 = True
+condicao2 = False
+
+if condicao1:
+    print('Condição 1 Verdadeira')
+elif condicao2:
+    print('Condição 2 Verdadeira')
+else:
+    print('Nenhuma condição verdadeira')
+
+print('Fim IF')
