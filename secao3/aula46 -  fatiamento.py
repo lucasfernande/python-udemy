c6b5fb71fa9403b5e6fea768611c3561a2405d1c
Lucas Fernandes <58313395+lucasfernande@users.noreply.github.com>
2023-04-27 17:21:52 -0300

    alguns fatiamentos de string

---
+++ secao3/aula46 -  fatiamento.py
@@ -0,0 +1,12 @@
+# 012345678
+# Olá Mundo
+#-987654321
+
+p = 'Olá Mundo'
+
+print(f'{p[2]}') # pegando o á com índice positivo
+print(f'{p[-7]}') # pegando o á com índice negativo
+print(f'{p[4:]}') # pegando do índice 4 pra frente
+print(f'{p[::2]}') # pegando de 2 em 2
+
+print(len(p)) # pegando a quantidade de caracteres
