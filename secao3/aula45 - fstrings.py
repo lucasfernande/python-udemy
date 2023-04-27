a8427b8c90da7c359b246f7e8fbeccce04034687
Lucas Fernandes <58313395+lucasfernande@users.noreply.github.com>
2023-04-27 17:11:33 -0300

    algumas funções f-strings

---
+++ secao3/aula45 - fstrings.py
@@ -0,0 +1,8 @@
+var = 'ABC'
+n = 1299.90
+
+print(f'{var: >10}') # coloca 10 espaços à esquerda da variável
+print(f'{var: <10} .') # coloca 10 espaços à direita da variável
+print(f'{var: ^10} .') # no centro de 10 espaços
+
+print(f'R$ {n:,.2f}')
