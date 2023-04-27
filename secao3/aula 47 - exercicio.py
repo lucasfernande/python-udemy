126405af6218ee4d9b398e39fa207931134b86e5
Lucas Fernandes <58313395+lucasfernande@users.noreply.github.com>
2023-04-27 17:33:59 -0300

    aula 47 - exercício

---
+++ secao3/aula 47 - exercicio.py
@@ -0,0 +1,12 @@
+nome = str(input('Digite seu nome: '))
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    print('Seu nome tem espaços') if ' ' in nome else print('Seu nome não tem espaços')
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é: {nome[0]}')
+    print(f'A última letra do seu nome é: {nome[-1]}')
+else:
+    print('Você deixou os campos vazios :(')
