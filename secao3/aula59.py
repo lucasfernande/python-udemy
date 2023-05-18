be23163d47709cffd872fc436049aa5087794896
lucasfernande <lucas.fernandes9415@outlook.com>
2023-05-18 17:45:42 -0300

    while e break

---
+++ secao3/aula59.py
@@ -0,0 +1,7 @@
+while True:
+    nome = str(input('Digite seu nome: '))
+
+    if nome == 'sair' or nome == 'Sair':
+        break # o break interrompe o laço imediatamente
+
+print('Fim do programa')
