5f5cd0457e892ee69930a340250254958584e931
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-08-30 19:33:16 -0300

    função print

---
+++ aula18.py
@@ -0,0 +1,10 @@
+print('Hello World!')
+print(2025)
+
+print(65, 12)
+
+print(18, 90, 30, sep='-')
+# O parâmetro sep define o que separa os elementos que o print vai imprimir. Por padrão, o python coloca um espaço vazio
+
+print('Teste', 43, end='###')
+# O parâmetro end define o que vem depois do print. Por padrão, o python coloca uma quebra de linha (\n)
