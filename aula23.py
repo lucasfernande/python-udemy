934ddb1c07dad6c808c346e7d81c120dff563b43
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-06 16:35:01 -0300

    usando variáveis

---
+++ aula23.py
@@ -0,0 +1,12 @@
+
+
+# As variáveis são utilizadas para guardar dados na memória do computador.
+# Segundo a PEP8, variáveis devem ter nomes iniciando com letras minúsculas, podemos usar números e underline(_) para separar palavras.
+
+nome = 'Maria'
+idade = 28
+maior_de_idade = idade >= 18 
+
+print(nome, 'tem', idade, 'anos')
+print(nome, 'é maior de idade?', maior_de_idade)
+
