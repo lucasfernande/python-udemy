80d24a31fe8387cdd628cb1881acdf59c030555d
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-06 15:35:04 -0300

    tipos de dados int e float

---
+++ aula20.py
@@ -0,0 +1,7 @@
+print(8, 5, 6, -5)
+print(type(8))
+# números do tipo int (inteiros, positivos e negativos)
+
+print(1.5, 7.6, -5.4)
+print(type(6.1))
+# números do tipo float (números com casa decimal)
