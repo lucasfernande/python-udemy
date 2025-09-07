0dfb4d516f0b0c6d3ad9fe29471a69403697ec38
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-07 17:54:52 -0300

    exercício de cálculo de imc

---
+++ exercicios/aula29_imc.py
@@ -0,0 +1,8 @@
+nome = 'Joãozinho';
+altura = 1.79
+peso = 80
+
+imc = peso / altura ** 2
+
+print(nome + ' tem ' + str(altura) + ' de altura,')
+print('pesa ' + str(peso) + ' kg e seu imc é ' + str(imc))
