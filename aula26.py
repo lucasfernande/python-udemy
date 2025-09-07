357fcddf488218d8389e35e889695a9992a900f4
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-07 17:35:20 -0300

    conhecendo operadores aritméticos

---
+++ aula26.py
@@ -0,0 +1,22 @@
+soma = 5 + 6
+print('Soma:', soma)
+
+subtracao = 15 - 8
+print('Subtração:', subtracao)
+
+multiplicacao = 2 * 8
+print('Multiplicação:', multiplicacao)
+
+divisao = 10 / 2
+print('Divisão:', divisao)
+# OBS: a divisão comum sempre retornará float, mesmo que seja composta por dois números inteiros.
+
+divisao_inteira = 9 // 5
+print('Divisão inteira:', divisao_inteira)
+# OBS: a divisão inteira sempre desconsidera as casas decimais e arredonda o resultado para baixo. Pode retornar float ou int
+
+exponenciacao = 2 ** 3
+print('Exponenciação:', exponenciacao)
+
+modulo = 25 % 3 # resto da divisão
+print('Módulo (resto da divisão):', modulo)
