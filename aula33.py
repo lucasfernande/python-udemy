7b0eb2dbe00f95ccea1fa3e1bd5c89a6a41e4467
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-11 20:24:16 -0300

    usando a função input para coletar dados do usuário

---
+++ aula33.py
@@ -0,0 +1,12 @@
+# nome = input('Digite seu nome: ')
+# print(f'Seu nome é {nome}!')
+# OBS: A função input sempre retornará uma string
+
+num1 = input('Digite um número: ')
+num2 = input('Digite outro número: ')
+
+int_num1 = int(num1)
+int_num2 = int(num2)
+# Convertendo para o tipo int
+
+print(f'A soma entre {num1} e {num2} é igual a {int_num1 + int_num2}.')
