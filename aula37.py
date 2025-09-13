47b2a9f50bb9c16c6d6ff5b09539f5daf102122a
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-12 22:35:18 -0300

    operadores relacionais (de comparação)

---
+++ aula37.py
@@ -0,0 +1,13 @@
+# Operadores relacionais
+# > - Maior
+# < - Menor
+# >= - Maior ou igual
+# <= - Menor ou igual
+# == - Igual
+# != - Diferente
+
+print(f'2 é maior que 1?: {2 > 1}')
+print(f'3 é menor que 5?: {3 < 5}')
+print(f'4 é maior ou igual a 4?: {4 >= 4}')
+print(f'8 é menor ou igual a 5?: {8 <= 5}')
+print(f'5 é diferente de 5?: {5 != 5}')
