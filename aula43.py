20366b41fec979f96273e8be3aa55343ca627b43
Lucas Fernandes <lucas.fernandes9415@outlook.com>
2025-09-14 17:59:14 -0300

    operadores in e not in

---
+++ aula43.py
@@ -0,0 +1,11 @@
+# Operadores in e not in
+
+# São utilizados para verificar a existência ou ausência de um elemento em uma string ou lista, por exemplo.
+
+# nome = 'Lucas'
+# print('a' in nome) # A letra "a" está presente na variável nome? -> True
+# print('r' not in nome) # A letra "r" NÃO está presente na variável nome? -> True 
+
+frase = 'Hello world!'
+if 'world' in frase:
+    print('A palavra world está na frase!')
